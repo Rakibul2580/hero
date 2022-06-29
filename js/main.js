3c85e32b2a2a0d3b0c811646333bced3ad4c3ff0
Rakibul  hasan <101660486+Rakibul2580@users.noreply.github.com>
2022-06-29 15:14:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,93 @@
+(function ($) {
+    "use strict";
+
+    $('#mobile-menu').meanmenu({
+        meanScreenWidth: "767",
+        meanMenuContainer: '.mobile-menu'
+    });
+
+
+    $('.slider-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+    $('.project-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+
+    $('.blog-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+
+    $('.brand-active').owlCarousel({
+        loop:true,
+        nav:false,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+        
+    });
+
+
+    document.addEventListener("DOMContentLoaded", function(){
+        /////// Prevent closing from click inside dropdown
+        document.querySelectorAll('.dropdown-menu').forEach(function(element){
+            element.addEventListener('click', function (e) {
+                e.stopPropagation();
+            });
+        })
+    }); 
+
+})(jQuery);
